8eeb37cef6d617c879a9c90a92515341af4b2a9f
couthi <couthi@example.com>
2026-01-19 09:57:30 +0100

    updated build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-dotnet publish -c Release -r linux-x64 --self-contained true -p:PublishSingleFile=true
+dotnet publish -c Release --self-contained true -p:PublishSingleFile=true
